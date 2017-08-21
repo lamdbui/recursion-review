7c2985fffda5249b382ed2acfa195fdae344c694
Hack Reactor Student <student@hackreactor.com>
2017-08-21 12:21:11 -0700

    Implement Json Stringify from scratch, recursively

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,29 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // String case
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  // Array case
+  if (Array.isArray(obj)) {
+    var results = [];
+    obj.forEach(item=> {
+      results.push(stringifyJSON(item));
+    });
+    return '[' + results.join(',') + ']';
+  }
+  // Object case
+  if ((obj !== null) && typeof obj === 'object') {
+    var results = [];
+    for (var key in obj) {
+      var value = obj[key];
+      if (value !== undefined && typeof value !== 'function') {
+        results.push([stringifyJSON(key), stringifyJSON(value)].join(':'));
+      }
+    }
+    return '{' + results.join(',') + '}';
+  }
+  // Base case
+  return '' + obj;
 };
